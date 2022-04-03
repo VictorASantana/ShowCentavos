708684e4f5177337b28e8b55f99e78ff63c5b3b4
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-04-03 14:42:46 -0300

    Exibe popup que indica erro ou acerto a cada questão

--- Home.py
+++ Home.py
@@ -1,4 +1,8 @@
-#Tela principal do aplicativo
+#Interface desenvolvida para o jogo ShowDeCentavos da disciplina de PCS3635
+#Nomes e NUSP:
+#Pedro Henrique Rodrigues Viveiros - 11804035
+#Pedro Vitor Bacic - 11806934
+#Victor de Almeida Santana -11806718
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -6,15 +10,22 @@ from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 #Variáveis Globais:
 global name
+#Variáveis a serem passadas para a FPGA
 global bitword
 global bitname
+#Variáveis usadas pela própria interface
 counter = 0
 score = 0
+popup_wrong = Popup(title='Errado', content=Label(text='Resposta errada'), size_hint=(None, None), size=(400, 400))
+popup_correct = Popup(title='Certo', content=Label(text='Resposta correta'), size_hint=(None, None),size=(400, 400))
+#Variáveis recebidas pela FPGA
 global correct
-
+correct = 0
 #Armazena as perguntas, acessadas via contador
 def questions(counter):
     questions = ["Qual bicho transmite Doença de Chagas?",
@@ -35,6 +46,7 @@ def questions(counter):
                  "Qual profissão utiliza uma ferramenta chamada formão?"]
     return questions[counter]
 
+#Armazena as respostas da Alternativa A
 def answerA(counter):
     answerA = ["Abelha",
                 "Caju",
@@ -54,6 +66,7 @@ def answerA(counter):
                 "carpinteiro"]
     return answerA[counter]
 
+#Armazena as respostas da Alternativa B
 def answerB(counter):
     answerB = ["Barata",
                 "Abóbora",
@@ -73,6 +86,7 @@ def answerB(counter):
                 "relojoeiro"]
     return answerB[counter]
 
+#Armazena as respostas da Alternativa C
 def answerC(counter):
     answerC = ["Pulga",
                  "Chuchu",
@@ -92,6 +106,7 @@ def answerC(counter):
                  "confeiteiro"]
     return answerC[counter]
 
+#Armazena as respostas da Alternativa D
 def answerD(counter):
     answerD = ["Barbeiro",
                  "Coco",
@@ -111,12 +126,13 @@ def answerD(counter):
                  "bombeiro"]
     return answerD[counter]
 
+#Verifica se a resposta escolhida está correta
 def verify():
     if correct == 1:
         global score
         score += 1
 
-
+#Controle da tela de login
 class Login(Screen):
     username = ObjectProperty(None)
     def btn(self):
@@ -128,6 +144,7 @@ class Login(Screen):
             bitname = 1
         print(bitname)
 
+#Controle da tela de jogo
 class Game(Screen):
     content = StringProperty("Pressione qualquer botão para iniciar")
     contentA = StringProperty()
@@ -144,10 +161,16 @@ class Game(Screen):
         self.contentC = "Alternativa C"
         self.contentD = "Alternativa D"
 
+
     def btnA(self):
         bitword = "1000"
         print(bitword)
         global counter
+        if counter > 0:
+            if correct == 0:
+                popup_wrong.open()
+            else:
+                popup_correct.open()
         if counter < 16:
             self.content = str(questions(counter))
             self.contentA = str(answerA(counter))
@@ -168,6 +191,11 @@ class Game(Screen):
         bitword = "0100"
         print(bitword)
         global counter
+        if counter > 0:
+            if correct == 0:
+                popup_wrong.open()
+            else:
+                popup_correct.open()
         if counter < 16:
             self.content = str(questions(counter))
             self.contentA = str(answerA(counter))
@@ -186,6 +214,11 @@ class Game(Screen):
         bitword = "0010"
         print(bitword)
         global counter
+        if counter > 0:
+            if correct == 0:
+                popup_wrong.open()
+            else:
+                popup_correct.open()
         if counter < 16:
             self.content = str(questions(counter))
             self.contentA = str(answerA(counter))
@@ -205,6 +238,11 @@ class Game(Screen):
         bitword = "0001"
         print(bitword)
         global counter
+        if counter > 0:
+            if correct == 0:
+                popup_wrong.open()
+            else:
+                popup_correct.open()
         if counter < 16:
             self.content = str(questions(counter))
             self.contentA = str(answerA(counter))
@@ -221,13 +259,15 @@ class Game(Screen):
 
 
 
-
+#Controle da tela de instruções
 class Instructions(Screen):
     pass
 
+#Controle da tela principal
 class Main(Screen):
     pass
 
+#Controle das telas
 class Home(ScreenManager):
     pass
 
